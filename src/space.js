6e4660450256aa9fd707bea4b18bb37b4990a732
Jekin Gohel <jekin1991@gmail.com>
2023-11-20 19:31:01 +0530

    Add files via upload

---
+++ src/space.js
@@ -0,0 +1,9 @@
+export default class Space {
+  constructor(spaceType, width, height, x, y) {
+    this.spaceType = spaceType
+    this.width = width
+    this.height = height
+    this.x = x
+    this.y = y
+  }
+}
